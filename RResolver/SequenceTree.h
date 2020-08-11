c92c512548280981bc336df013ebf7834583ee32
schutzekatze <vladimir.nik94@gmail.com>
2020-08-11 16:29:07 -0700

    clang-format RResolver files

--- RResolver/SequenceTree.h
+++ RResolver/SequenceTree.h
@@ -1,14 +1,17 @@
 #ifndef RRESOLVER_SEQUENCETREE_H
 #define RRESOLVER_SEQUENCETREE_H 1
 
-#include "Contigs.h"
 #include "Common/Sequence.h"
+#include "Contigs.h"
 
 #include <list>
 
 std::list<Sequence>
-getTreeSequences(const ContigNode& start,
-                 const int overlap, const int maxLength,
-                 const bool forward, const int maxPaths);
+getTreeSequences(
+    const ContigNode& start,
+    const int overlap,
+    const int maxLength,
+    const bool forward,
+    const int maxPaths);
 
 #endif
